83f0d75374f11c53503d1f5b66a10d582cec5f5b
Marc Tardif <marc@interunion.ca>
2013-11-03 17:27:29 -0500

    Fixed year in license information.

--- jiraban/board.py
+++ jiraban/board.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2012, Marc Tardif <marc@interunion.ca>
+# Copyright (c) 2013, Marc Tardif <marc@interunion.ca>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
